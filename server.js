08bc412a56151e42569db3d5f2aa4b3d1c2840ef
wafflesJ <jacobsamuelmills7@gmail.com>
2024-12-19 11:24:24 -0500

    added

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const http = require("http");
 
-// Create an HTTP server
 const server = http.createServer((req, res) => {
-  console.log(req.query.link);
+  if (req.method === 'GET' && req.url === '/example') {
+    res.statusCode = 200;
+    res.end(res.end(https.get('https://www.google.ca/?safe=active&ssui=on')));
+  } else {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello, Render!\n");
+  res.end("Test\n");
+}
+  //res.end(https.get('https://link.com'));
+
 });
 
-// Use the PORT environment variable or default to 3000
+
+
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
