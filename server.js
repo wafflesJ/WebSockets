5c90ddf29f45803abad7bbad2d6b188188b33b56
wafflesJ <jacobsamuelmills7@gmail.com>
2024-12-19 11:50:22 -0500

    https to http

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/test') {
     res.statusCode = 200;
-    res.end(https.get('https://www.york.ac.uk/teaching/cws/wws/webpage1.html'));
+    res.end(http.get('https://www.york.ac.uk/teaching/cws/wws/webpage1.html'));
   } else {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Main html here\n");
