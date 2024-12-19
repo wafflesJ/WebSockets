d35d27446ed6a450c8a469eb1487a2fd9f26ca73
wafflesJ <jacobsamuelmills7@gmail.com>
2024-12-19 12:31:05 -0500

    big code

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/test') {
-    res.statusCode = 200;
-    res.end(http.get('https://www.york.ac.uk/teaching/cws/wws/webpage1.html'));
-  } else {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Main html here\n");
-}
+res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+
+    // Make the HTTP request
+    https.get('https://www.york.ac.uk/teaching/cws/wws/webpage1.html', (response) => {
+      let data = '';
+
+      // Collect data from the HTTP request
+      response.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      // Once the data is fully received, send it to the client
+      response.on('end', () => {
+        res.end(data); // Send the HTML content to the client
+      });
+
+    }).on('error', (err) => {
+      // Handle error if the request fails
+      res.statusCode = 500;
+      res.end('Error: ' + err.message);
+    });
+
   
 
 });
