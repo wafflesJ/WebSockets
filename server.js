e9540af131418d4229b7a4dfe4f65db581b0da1a
wafflesJ <jacobsamuelmills7@gmail.com>
2024-12-19 15:19:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
 const http = require("http");
-const https = require("https"); // Include the https module
-const url = require("url");
+const https = require("https");
+const url = require("url"); // Import the 'url' module for parsing URLs
+const querystring = require("querystring"); // For URL decoding
 
 const server = http.createServer((req, res) => {
-  
+  // Parse the request URL
   const parsedUrl = url.parse(req.url, true);
   const pathParts = parsedUrl.pathname.split('/').filter(Boolean);
-  
+
   if (req.method === 'GET' && pathParts[0] === 'test' && pathParts[1]) {
+    // Decode the path variable to get the target URL
+    const targetUrl = decodeURIComponent(pathParts[1]);
+    
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
 
-    // Make the HTTP request
-    https.get(pathParts[1], (response) => {
+    // Make the HTTPS request to the decoded URL
+    https.get(targetUrl, (response) => {
       let data = '';
 
-      // Collect data from the HTTP request
       response.on('data', (chunk) => {
         data += chunk;
       });
 
-      // Once the data is fully received, send it to the client
       response.on('end', () => {
         res.end(data); // Send the HTML content to the client
       });
-
     }).on('error', (err) => {
-      // Handle error if the request fails
       res.statusCode = 500;
       res.end('Error: ' + err.message);
     });
