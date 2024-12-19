ea40fdc8bf75ac8b3600faf29e0e58713c82fc2d
wafflesJ <jacobsamuelmills7@gmail.com>
2024-12-19 11:27:47 -0500

    better link

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/example') {
     res.statusCode = 200;
-    res.end(res.end(https.get('https://www.google.ca/?safe=active&ssui=on')));
+    res.end(res.end(https.get('https://www.google.ca/')));
   } else {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Test\n");
