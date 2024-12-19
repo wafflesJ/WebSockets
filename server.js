77763e662fc20ff48b97edef56f56775f2c0cee6
wafflesJ <jacobsamuelmills7@gmail.com>
2024-12-19 08:46:22 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require("http");
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello, Render!\n");
+});
+
+// Use the PORT environment variable or default to 3000
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
